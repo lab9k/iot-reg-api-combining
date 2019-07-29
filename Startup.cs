dcffe92dd1764866cc6d57392699090ded5abf5d
Jefwillems <jefwillems100@gmail.com>
2019-07-29 20:59:09 +0200

    Add openapi specification
    Signed-off-by: Jefwillems <jefwillems100@gmail.com>

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace iot_reg_combining_backend {
   public class Startup {
@@ -15,6 +16,7 @@ namespace iot_reg_combining_backend {
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services) {
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo {Title = "IOT-registry api", Version = "v1"}));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +28,12 @@ namespace iot_reg_combining_backend {
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
       }
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "IOT-registry api v1");
+        c.RoutePrefix = string.Empty;
+      });
 
       app.UseHttpsRedirection();
       app.UseMvc();
